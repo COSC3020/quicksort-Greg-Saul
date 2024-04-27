a3fcc31a29d2691a222fc3210cea279c58cf0acb
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-26 19:51:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,58 +8,51 @@ function partition(arr, low, high) {
 
     for (let j = low; j < high; j++) {
         if (arr[j] <= pivot) {
-            //swaps them
             i++;
-            let temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            swap(arr, i, j)
         }
     }
-
-    //sorts the pivot into place
-    let temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    // console.log(arr)
+    swap(arr, i + 1, high)
     return ++i;
 }
 
 function quicksort(arr) {
     if(arr.length <= 1){
-        return arr;
+        return arr
     }
-    let stack = [];
-    let top = -1;
-    stack[++top] = 0; //lower bound
-    stack[++top] = arr.length - 1; //upper bound
-    // console.log(stack[top])
-
-    while (top >= 0) {
-        // console.log(stack)
-        let hi = stack[top--];
-        let lo = stack[top--];
-
-        let pivot = partition(arr, lo, hi);
-        // console.log(pivot)
-
-        if (pivot - 1 > lo) {
-            stack[++top] = lo;
-            stack[++top] = pivot - 1;
-        }
-
-        if (pivot + 1 < hi) {
-            stack[++top] = pivot + 1;
-            stack[++top] = hi;
+    const stack = [];
+    stack.push(0);
+    stack.push(arr.length - 1);
+    while (stack.length > 0) {
+        let high = stack.pop();
+        let low = stack.pop();
+        if (low < high) {
+            let pivot = partition(arr, low, high);
+            if(pivot - 1 > low){
+                stack.push(low);
+                stack.push(pivot - 1);
+            }
+            if(pivot + 1 < high){
+                stack.push(pivot + 1);
+                stack.push(high);
+            }
+            
         }
     }
-    return arr;
+    return arr
 }
 
-    // let arr = [ 83, 92, 44, 78, 30, 56, 79, 90, 35, 87, 9, 88, 62, 75, 61, 11, 19, 81, 71, 53 ]
-    // let arr = [ 4, 3, 5, 2, 1, 3, 2, 3 ];
-    // let arr = [8,7,6,5,4,3,2,1] 
-    // let arr = [0,0]
-    // quicksort(arr); 
 
-    // console.log(arr)
+function swap(a,i,j){
+    let tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+}
+
+// let arr = [ 83, 92, 44, 78, 30, 56, 79, 90, 35, 87, 9, 88, 62, 75, 61, 11, 19, 81, 71, 53 ]
+// let arr = [ 4, 3, 5, 2, 1, 3, 2, 3 ];
+// let arr = [8,7,6,5,4,3,2,1] 
+// let arr = [0,0]
+// quicksort(arr); 
+
+// console.log(arr)
